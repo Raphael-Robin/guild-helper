72a407491669e1f94e0178ab1694b1a97f984bfc
Arepha <raphael.robin31000@gmail.com>
2026-03-03 11:55:20 +0100

    wip lootsplits

--- src/Interfaces/ILootsplitManager.py
+++ src/Interfaces/ILootsplitManager.py
@@ -4,15 +4,15 @@ from src.Model import Player, Lootsplit
 
 class ILootsplitManager(ABC):
     @abstractmethod
-    def create_lootsplit(
+    async def create_lootsplit(
         self, item_value: int, silver: int, repair_cost: int
     ) -> Lootsplit:
         pass
 
     @abstractmethod
-    def add_players(self, players: list[Player]) -> None:
+    async def add_players(self, players: list[Player]) -> None:
         pass
 
     @abstractmethod
-    def add_balances(self) -> None:
+    async def add_balances(self) -> None:
         pass
--- src/Services/LootsplitManager.py
+++ src/Services/LootsplitManager.py
@@ -1,5 +1,29 @@
-from src.Interfaces.ILootsplitManager import ILootsplitManager
+from src.Interfaces import ILootsplitManager, IConfigurationManager, IDatabaseManager
+from src.Model import Player, Lootsplit
 
 
 class LootsplitManager(ILootsplitManager):
-    pass  # TODO
+    def __init__(
+        self,
+        configuration_manager: IConfigurationManager,
+        database_manager: IDatabaseManager,
+    ) -> None:
+        self.configuration_manager = configuration_manager
+        self.database_manager = database_manager
+
+    async def create_lootsplit(
+        self, item_value: int, silver: int, repair_cost: int
+    ) -> Lootsplit:
+        return Lootsplit(
+            configuration=await self.configuration_manager.get_config(),
+            players=[],
+            item_value=item_value,
+            silver=silver,
+            repair_cost=repair_cost,
+        )
+
+    def add_players(self, players: list[Player]) -> None:
+        pass
+
+    def add_balances(self) -> None:
+        pass
